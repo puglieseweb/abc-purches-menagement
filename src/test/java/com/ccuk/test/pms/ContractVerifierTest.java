8730d442939b785280c5ba9772acb4cb19e6ebdd
pweb <puglieseweb@gmail.com>
2018-11-11 22:34:19 +0000

    First draft of the RestFul  API

--- src/test/java/com/ccuk/test/pms/ContractVerifierTest.java
+++ src/test/java/com/ccuk/test/pms/ContractVerifierTest.java
@@ -1,8 +1,5 @@
 package com.ccuk.test.pms;
 
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
-import com.revinate.assertj.json.JsonPathAssert;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import io.restassured.response.ResponseOptions;
 import org.junit.Before;
@@ -15,7 +12,6 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.context.WebApplicationContext;
 
-import static com.jayway.jsonpath.matchers.JsonPathMatchers.withJsonPath;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,21 +27,63 @@ public class ContractVerifierTest {
 
     @Before
     public void setUp() {
+        // Requirement 1. Create a simple order management system for ABC online shop, where customer should be able to purchase items online.
         RestAssuredMockMvc.webAppContextSetup(this.context);
     }
 
+    // Requirement 5. The System should be able to list all orders for a customer
     @Test
-    public void validate_shouldReturnEvenWhenRequestParamIsEven() {
+    public void shouldFetchAllOrderForCustomer() {
         // when:
-        ResponseOptions response = given().get("/purchase-management-system/item/10001");
+        ResponseOptions response = given().get("/v1/com/abc/customers/001/orders/");
+        // then:
+        assertThat(response.statusCode()).isEqualTo(204);
+        // and:
+        final JsonPathAssertionUtil jsonPathUtil = new JsonPathAssertionUtil(response.getBody().asString());
+        jsonPathUtil.assertJsonPathHasValue("$.customer.id", "001");
+        jsonPathUtil.assertJsonPathHasValues("$.customer.orders", "[]");
+    }
+
+    //Requirement 4. The System should allow user to create a new order
+    @Test
+    public void shouldCreateNewOrder() {
+        // when:
+        ResponseOptions response = given().post("/v1/com/abc/customers/001/orders/");
+        // then:
+        assertThat(response.statusCode()).isEqualTo(201);
+        assertThat(response.header("Location")).isEqualTo(001);
+    }
+
+    // Requirement 6. The System should be able to fetch a specific order
+    @Test
+    public void shouldFetchASpecificOrder() {
+        // when:
+        ResponseOptions response = given().get("/v1/com/abc/customers/001/orders/001");
         // then:
         assertThat(response.statusCode()).isEqualTo(200);
         // and:
-        DocumentContext responseBody = JsonPath.parse(response.getBody().asString());
-        JsonPathAssert.assertThat(responseBody).jsonPathAsString("$.itemId").contains("10001");
-        JsonPathAssert.assertThat(responseBody).jsonPathAsString("$.itemName").contains("100 Watt");
-        JsonPathAssert.assertThat(responseBody).jsonPathAsString("$.itemDescription").contains("Bulb");
+        final JsonPathAssertionUtil jsonPathUtil = new JsonPathAssertionUtil(response.getBody().asString());
+        jsonPathUtil.assertJsonPathHasValue("$.order.id", "001");
+        jsonPathUtil.assertJsonPathHasValue("$.order.customerId", "001");
+        jsonPathUtil.assertJsonPathHasValue("$.order.items", "[]");
+    }
 
+    // Requirement 2. The System should allow user to add items to a basket.
+    @Test
+    public void shouldAddItemAnOrder() {
+        // when:
+        ResponseOptions response = given().post("/v1/com/abc/customers/001/orders/001/items");
+        // then:
+        assertThat(response.statusCode()).isEqualTo(201);
+        assertThat(response.header("Location")).isEqualTo(001);
+    }
 
+    // Requirements 3. The System should allow user to delete items from a basket.
+    @Test
+    public void shouldDeleteItemToOrder() {
+        // when:
+        ResponseOptions response = given().delete("/v1/com/abc/customers/001/orders/001/items/001");
+        // then:
+        assertThat(response.statusCode()).isEqualTo(204);
     }
 }
---
+++ src/test/java/com/ccuk/test/pms/JsonPathAssertionUtil.java
@@ -0,0 +1,25 @@
+package com.ccuk.test.pms;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JsonPathAssertionUtil {
+
+
+    private final String jsonString;
+
+    public JsonPathAssertionUtil(final String jsonString) {
+        this.jsonString = jsonString;
+
+    }
+
+    public void assertJsonPathHasValue(final String jsonPath, final String value) {
+        assertThat((String) JsonPath.read(jsonString, jsonPath)).isEqualTo(value);
+    }
+
+    public void assertJsonPathHasValues(String jsonPath, String value) {
+        assertThat((String) JsonPath.read(jsonString, jsonPath)).isEqualTo(value);
+    }
+}
